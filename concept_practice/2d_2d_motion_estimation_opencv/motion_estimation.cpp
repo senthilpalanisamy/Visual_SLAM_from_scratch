0269c56452b7843adf9ddd2667adf991cceefa49
senthil palanisamy <senthillihtnes1994@gmail.com>
2021-11-30 09:15:14 -0800

    Tested triangulation code from openCV

--- concept_practice/2d_2d_motion_estimation_opencv/motion_estimation.cpp
+++ concept_practice/2d_2d_motion_estimation_opencv/motion_estimation.cpp
@@ -47,6 +47,49 @@ cv::Point2d pixel2Cam(const cv::Point2d& point, const cv::Mat& K)
   return normalisedPoint;
 }
 
+void triangulate(const vector<cv::KeyPoint>& kp1, const vector<cv::KeyPoint>& kp2,
+                 const vector<cv::DMatch> &matches, const cv::Mat &R, const cv::Mat &t,
+                 cv::Mat &K, vector<cv::Point3d>& points3D)
+{
+  cv::Mat T1 = (cv::Mat_<double>(3, 4)<<
+           1.0, 0, 0, 0,
+           0, 1.0, 0, 0,
+           0, 0, 1.0, 0);
+  cv::Mat T2 = (cv::Mat_<double>(3, 4)<<
+           R.at<double>(0, 0), R.at<double>(0, 1), R.at<double>(0, 2), t.at<double>(0, 0),
+           R.at<double>(1, 0), R.at<double>(1, 1), R.at<double>(1, 2), t.at<double>(1, 0),
+           R.at<double>(2, 0), R.at<double>(2, 1), R.at<double>(2, 2), t.at<double>(2, 0));
+  cout<<"T value"<<T2.at<double>(0, 3)<<endl<<T2.at<double>(1, 3)<<endl<<T2.at<double>(2, 3)<<endl<<T2.at<double>(0,0)<<endl;
+  vector<cv::Point2d> points1, points2;
+
+  for(auto& match: matches)
+  {
+    auto normalised_point1 = pixel2Cam(kp1[match.queryIdx].pt, K);
+    auto normalised_point2 = pixel2Cam(kp2[match.trainIdx].pt, K);
+
+    points1.emplace_back(normalised_point1.x, normalised_point1.y);
+    points2.emplace_back(normalised_point2.x, normalised_point2.y);
+  }
+
+  cv::Mat points_4d;
+
+  cv::triangulatePoints(T1, T2, points1, points2, points_4d);
+
+  cout<<"printing points"<<endl;
+  //for(cv::Point3d& point:points4D)
+  for(int i=0; i < points_4d.cols; ++i)
+  {
+    cv::Mat point = points_4d.col(i);
+    point /= point.at<double>(3, 0);
+    points3D.emplace_back(point.at<double>(0, 0), point.at<double>(1, 0),
+                         point.at<double>(2, 0));
+    cout<<point.at<double>(0, 0)<<" "<<point.at<double>(1, 0)<<" "<<point.at<double>(2, 0)
+        <<endl;
+  }
+
+}
+
+
 int main()
 {
   const cv::Mat img1 = cv::imread("1.png", 0);
@@ -64,7 +107,7 @@ int main()
 
 
   cout<<"epipolar constraint error"<<endl;
-  for(auto& match: matches)
+  for(const auto& match: matches)
   {
     cv::Point2d point1 = pixel2Cam(kp1[match.queryIdx].pt, K);
     cv::Point2d point2 = pixel2Cam(kp2[match.trainIdx].pt, K);
@@ -74,6 +117,12 @@ int main()
     cv::Mat error = y1.t() * t_xR * y2;
     cout<<error.t()<<endl;
   }
+
+  vector<cv::Point3d> points3D;
+  triangulate(kp1, kp2, matches, R, t, K, points3D);
+
+
+
 
 
 
